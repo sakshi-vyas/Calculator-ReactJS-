23e02aea2eebb3ee3b69dfb8c4df5851046a9071
sakshi-vyas <sakshivyas216@gmail.com>
2020-08-22 09:30:32 +0530

    Updates

---
+++ precache-manifest.ec9c08ac7a963ab456ceedfd70135ad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff24923678cea938dcc12c64bd689e91",
+    "url": "/Calculator-ReactJS-/index.html"
+  },
+  {
+    "revision": "47df97706eb3bc0c4f4d",
+    "url": "/Calculator-ReactJS-/static/css/main.890c15c7.chunk.css"
+  },
+  {
+    "revision": "114bbaea14242877659e",
+    "url": "/Calculator-ReactJS-/static/js/2.6912228e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator-ReactJS-/static/js/2.6912228e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47df97706eb3bc0c4f4d",
+    "url": "/Calculator-ReactJS-/static/js/main.f6f810a3.chunk.js"
+  },
+  {
+    "revision": "5c2591f7c7b53b1d70ec",
+    "url": "/Calculator-ReactJS-/static/js/runtime-main.b1727a04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Calculator-ReactJS-/static/media/logo.5d5d9eef.svg"
+  }
+]);
